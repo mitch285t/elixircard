7a9e7b212c50db46cda73780bd92acca6fe99e7d
Mitchell Williams <mscottwilliams1994@gmail.com>
2019-12-28 11:49:38 -0600

    added doctests and documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,35 +1,67 @@
 defmodule Cards do
-  def create_deck do
-  values =  ["Ace", "Two", "Three", "Four", "Five"]
-  suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
 
-    for suit <- suits, value <- values do 
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
-
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+@doc """
+Determines whether a deck has a given card or not 
 
-  def contains?(deck, card) do 
+  ## Examples
+      
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+
+"""
+  def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
-  def deal(deck, hand_size) do 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
+  def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
-  def save(deck, filename) do 
+  def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
-  def load(filename) do 
+
+  def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term binary
       {:error, _reason} -> "That file does not exist"
     end
+  end
 
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
